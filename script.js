960faabe31a5cd383aa1b422a12f0292d8c20c88
Małgorzata Nowak <goskanowak87@gmail.com>
2018-06-16 14:58:28 +0200

    Change space in script

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ class Stopwatch {
         this.reset();
         this.print(this.times);
     }
-      reset() {
+    
+    reset() {
         this.times = {
             minutes: 0,
             seconds: 0,
             miliseconds: 0
         };
     }
+    
     print() {
         this.display.innerText = this.format(this.times);
     }
@@ -19,10 +21,11 @@ class Stopwatch {
     format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     }
+    
     start() {
-    if (!this.running) {
-        this.running = true;
-        this.watch = setInterval(() => this.step(), 10);
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
         }
     }
 
@@ -51,7 +54,7 @@ class Stopwatch {
 }
 
 const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
+    document.querySelector('.stopwatch'));
 
 let startButton = document.getElementById('start');
 startButton.addEventListener('click', () => stopwatch.start());
